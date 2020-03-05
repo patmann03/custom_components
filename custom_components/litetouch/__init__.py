2ed7d03f9a243957753406729f3aaff1bb32e093
patmann03 <patrick.carr03@gmail.com>
2020-03-04 22:40:36 -0600

    add switch

--- custom_components/litetouch/__init__.py
+++ custom_components/litetouch/__init__.py
@@ -26,7 +26,7 @@ ENTITY_SIGNAL = "litetouch_entity_{}"
 
 
 CONF_DIMMERS = "dimmers"
-CONF_SWITCH = "keypads"
+CONF_SWITCH = "switch"
 CONF_ADDR = "addr"
 CONF_LOADID = "loadid"
 
@@ -39,8 +39,12 @@ DIMMER_SCHEMA = vol.Schema(
     }
 )
 
-KEYPAD_SCHEMA = vol.Schema(
-    {vol.Required(CONF_ADDR): cv.string, vol.Required(CONF_NAME): cv.string}
+SWITCH_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_ADDR): cv.string,
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_LOADID): cv.string,
+    }
 )
 
 CONFIG_SCHEMA = vol.Schema(
@@ -51,7 +55,7 @@ CONFIG_SCHEMA = vol.Schema(
                 vol.Required(CONF_PORT): cv.port,
                 vol.Required(CONF_DIMMERS): vol.All(cv.ensure_list, [DIMMER_SCHEMA]),
                 vol.Optional(CONF_SWITCH, default=[]): vol.All(
-                    cv.ensure_list, [KEYPAD_SCHEMA]
+                    cv.ensure_list, [SWITCH_SCHEMA]
                 ),
             }
         )
@@ -66,14 +70,19 @@ def setup(hass, base_config):
     def hw_callback(msg_type, values):
         """Dispatch state changes."""
         _LOGGER.debug("callback: %s, %s", msg_type, values)
+        # set entity id eq to callback value.
         addr = values[0]
         signal = ENTITY_SIGNAL.format(addr)
-        _LOGGER.debug("send to dispatcher %s, %s, %s", signal, msg_type, values)
+
+        _LOGGER.debug("send to dispatcher %s, %s, %s,", signal, msg_type, values)
+
         dispatcher_send(hass, signal, msg_type, values)
 
     config = base_config.get(DOMAIN)
     _LOGGER.debug("Initiating connect to controller")
+
     controller = LiteTouch(config[CONF_HOST], config[CONF_PORT], hw_callback)
+
     hass.data[LITETOUCH_CONTROLLER] = controller
 
     def cleanup(event):
@@ -84,10 +93,8 @@ def setup(hass, base_config):
     dimmers = config[CONF_DIMMERS]
     load_platform(hass, "light", DOMAIN, {CONF_DIMMERS: dimmers}, base_config)
 
-    for key_config in config[CONF_SWITCH]:
-        addr = key_config[CONF_ADDR]
-        name = key_config[CONF_NAME]
-        LiteTouchKeypadEvent(hass, addr, name)
+    switch = config[CONF_SWITCH]
+    load_platform(hass, "switch", DOMAIN, {CONF_SWITCH: switch}, base_config)
 
     return True
 
@@ -123,32 +130,3 @@ class LiteTouchDevice:
         """No need to poll."""
         return False
 
-
-class LiteTouchKeypadEvent:
-    """When you want signals instead of entities.
-
-    Stateless sensors such as keypads are expected to generate an event
-    instead of a sensor entity in hass.
-    """
-
-    def __init__(self, hass, addr, name):
-        """Register callback that will be used for signals."""
-        self._hass = hass
-        self._addr = addr
-        self._name = name
-        self._id = slugify(self._name)
-        signal = ENTITY_SIGNAL.format(self._addr)
-        async_dispatcher_connect(self._hass, signal, self._update_callback)
-
-    @callback
-    def _update_callback(self, msg_type, values):
-        """Fire events if button is pressed or released."""
-        _LOGGER.debug("Update Callback keypad %s, %s", msg_type, values)
-        if msg_type == HW_BUTTON_PRESSED:
-            event = EVENT_BUTTON_PRESS
-        elif msg_type == HW_BUTTON_RELEASED:
-            event = EVENT_BUTTON_RELEASE
-        else:
-            return
-        data = {CONF_ID: self._id, CONF_NAME: self._name, "button": values[1]}
-        self._hass.bus.async_fire(event, data)
--- custom_components/litetouch/switch.py
+++ custom_components/litetouch/switch.py
@@ -1,17 +1,17 @@
-"""Support for LiteTouch lights."""
+"""Support for LiteTouch Switch."""
 import logging
 
 import pylitetouch.pylitetouch
 import time
 
-from homeassistant.components.light import ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light
+from homeassistant.components.switch import SwitchDevice
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 
 from . import (
     CONF_ADDR,
-    CONF_DIMMERS,
+    CONF_SWITCH,
     CONF_LOADID,
     ENTITY_SIGNAL,
     LITETOUCH_CONTROLLER,
@@ -22,29 +22,28 @@ _LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_entities, discover_info=None):
-    """Set up LiteTouch lights."""
+    """Set up LiteTouch Switch."""
     if discover_info is None:
         return
 
     controller = hass.data[LITETOUCH_CONTROLLER]
     devs = []
-    for dimmer in discover_info[CONF_DIMMERS]:
-        dev = LiteTouchLight(
-            controller, dimmer[CONF_ADDR], dimmer[CONF_NAME], dimmer[CONF_LOADID]
+    for switch in discover_info[CONF_SWITCH]:
+        dev = LiteTouchSwitch(
+            controller, switch[CONF_ADDR], switch[CONF_NAME], switch[CONF_LOADID]
         )
         devs.append(dev)
     add_entities(devs, True)
 
 
-class LiteTouchLight(LiteTouchDevice, Light):
-    """LiteTouch Light."""
+class LiteTouchSwitch(LiteTouchDevice, SwitchDevice):
+    """LiteTouch Switch."""
 
     def __init__(self, controller, addr, name, loadid):
         """Create device with Addr, name, and loadid."""
         super().__init__(controller, addr, name)
         self._loadid = int(loadid)
-        self._level = 0
-        self._prev_level = 0
+        self._state = 0
 
     async def async_added_to_hass(self):
         """Call when entity is added to hass."""
@@ -53,36 +52,13 @@ class LiteTouchLight(LiteTouchDevice, Light):
         async_dispatcher_connect(self.hass, signal, self._update_callback)
         self._controller.get_led_states(self._addr)
 
-    @property
-    def supported_features(self):
-        """Supported features."""
-        return SUPPORT_BRIGHTNESS
-
     def turn_on(self, **kwargs):
-        """Turn on the light."""
-        # _LOGGER.debug("Call turn on %s", **kwargs)
-        if ATTR_BRIGHTNESS in kwargs:
-            new_level = kwargs[ATTR_BRIGHTNESS]
-        elif self._prev_level == 0:
-            new_level = 255
-        else:
-            new_level = self._prev_level
-        self._set_brightness(new_level)
+        """Turn on the switch."""
+        self._controller.set_loadon(self._loadid)
 
     def turn_off(self, **kwargs):
-        """Turn off the light."""
-        self._set_brightness(0)
-
-    @property
-    def brightness(self):
-        """Control the brightness."""
-        return self._level
-
-    def _set_brightness(self, level):
-        """Send the brightness level to the device."""
-        lid = str(self._loadid)
-        _LOGGER.debug("Set Brightness on loadid %s, %s", lid, level)
-        self._controller.set_loadlevel(self._loadid, int((level * 100) / 255))
+        """Turn off the Switch."""
+        self._controller.set_loadoff(self._loadid)
 
     @property
     def device_state_attributes(self):
@@ -91,21 +67,19 @@ class LiteTouchLight(LiteTouchDevice, Light):
 
     @property
     def is_on(self):
-        """Is the light on/off."""
-        return self._level != 0
+        """Is the Switch on/off."""
+        return self._state != 0
 
     @callback
     def _update_callback(self, msg_type, values):
         """Process device specific messages."""
-        _LOGGER.debug("Light Callback: %s, %s", msg_type, values)
 
         if msg_type == "RLEDU" or msg_type == "CGLES":
-
+            _LOGGER.debug("Switch Update Callback: %s, %s", msg_type, values)
             lvl = int(values[1])
             if lvl == 1:
-                self._level = 255
+                self._state = 1
             else:
-                self._level = 0
-            if self._level != 0:
-                self._prev_level = self._level
+                self._state = 0
+
             self.async_schedule_update_ha_state()
